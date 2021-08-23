c312645b7b9ba13eb445776b3a3dee03277be6c2
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-23 14:02:25 +0800

    exercise ten

---
+++ exerciseTen.py
@@ -0,0 +1,12 @@
+# four different string variables
+tabby_cat = "\t I'm tabbed in."
+persion_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = "I'll do a list: \t* Cat Fish \t* Fishes \t* Catnip\n\t* Grass"
+
+
+# the following codes print out the variables 
+print(tabby_cat)
+print(persion_cat)
+print(backslash_cat)
+print(fat_cat)
