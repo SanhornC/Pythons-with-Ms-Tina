3ea1a3a12c13ff4472d3df05bb9d2e04f66c86ad
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-23 13:38:44 +0800

    exercise seven

---
+++ exerciseSeven.py
@@ -0,0 +1,37 @@
+print("Mary had a little lamb.")
+# print out the wordings between the ()
+print("Its fleece was white as {}.".format('snow'))
+# .format() helps to put the string into the {}
+print("And everywhere that Mary went.")
+# print out the wordings
+print("." * 10)  # what'd that do?
+# print out 10 dots
+
+end1 = "C"
+# end1 string variable
+end2 = "h"
+# end2 string variable
+end3 = "e"
+# end3 string variable
+end4 = "e"
+# end4 string variable
+end5 = "s"
+# end5 string variable
+end6 = "e"
+# end6 string variable
+end7 = "B"
+# end7 string variable
+end8 = "u"\
+# end8 string variable
+end9 = "r"
+# end9 string variable
+end10 = "g"
+# end10 string variable
+end11 = "e"
+# end11 string variable
+end12 = "r"
+# end12 string variable
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# end = " " provides a space between cheese and burger
+print(end7 + end8 + end9 + end10 + end11 + end12)
+# print the burger
