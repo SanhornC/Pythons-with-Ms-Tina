5366c5720c8bcc8dabe5e4058e121e5a2e4b72ee
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-23 13:53:29 +0800

    exercise Nine

---
+++ exerciseNine.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# string variable days 
+months = "Jan \n Feb \n Mar \n Apr \n May \n Jun \n Jul \n Aug"
+# \n between each month stands for endl 
+print ("Here are the days: ", days)
+# print out the days
+print ("Here are the months: ", months)
+# print out the months
+
+
+print("There's something going on here. \n With the three double-quotes \n We will be able to type as much as we like \n Even 4 lines if we want, 0r 5, or 6.")
+# print out the sentences 
