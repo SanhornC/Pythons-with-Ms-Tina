89efeb4145e8ccb7ffba73ef79c7ea093e0398d5
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-26 14:53:19 +0800

    exercise 17

--- ex17.py
+++ ex17.py
@@ -5,13 +5,12 @@ script, from_file, to_file = argv
 
 print("Copying from %s to %s" %(from_file, to_file))
 
-in_file = open(from_file)
-indata = in_file.read()
+indata = in_file = open(from_file).read()
 
 print("The input file is %d bytes long" %len(indata))
 
 print("Does the output file exist? %r" %exists(to_file))
-
+# exists() method in Python is used to check whether the specified path exists or not. 
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input();
 out_file = open(to_file, 'w')
@@ -20,5 +19,7 @@ out_file.write(indata)
 
 print("Alright, all done.")
 out_file.close()
-in_file.close()
+
+#as a file is opened in write, it is locked for writing. so if you dont close it, you cant ahve access to the file outside the program while it's open in python
+
 
