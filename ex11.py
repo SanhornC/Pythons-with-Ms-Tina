dbd407993a645b1b349c25719c0e92bf8929461b
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-28 23:53:02 +0800

    exercise 11

---
+++ ex11.py
@@ -0,0 +1,25 @@
+print("How old are you? ", end = " ")
+age = input()
+
+print("How tall are you?", end = " ")
+height = input()
+
+print("How much do you weigh?", end = " ")
+weight = input()
+
+print("So, you're %r old, %r tall, %r heavy." %(age, height, weight))
+
+
+# drill 3
+grade = input("What did you get? ")
+print(grade)
+
+
+# drill 1
+# in python3, raw_input has been replaced by simply input. Basically input is for users to enter stuff
+
+# drill 2
+# sys.stdin.readline() is an alternative way of input()
+
+# drill 4
+print('6\'2"')
