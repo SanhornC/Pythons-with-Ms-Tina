b27799a5c9fd8f93c9b28a967c3f59bdc2c75cfb
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-29 00:03:09 +0800

    exercise 13

--- ex13.py
+++ ex13.py
@@ -1,9 +1,15 @@
+
+#imports commands
 from sys import argv
 
-script, first, second, third = argv
+
+#unpacked arguments
+script, second, third = argv
 
 
+# print things
 print("The script is called: ", script)
-print("The first is called: ", first)
 print("The second is called: ", second)
 print("The third is called: ", third)
+
+#deleted due to fewer argument on study drill
