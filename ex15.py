aa5dc78017c7497a5c8b68a2e68319650147d584
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-29 00:13:11 +0800

    exercise 15

--- ex15.py
+++ ex15.py
@@ -1,16 +1,23 @@
+# import things
 from sys import argv
 
+# unpacked things
 script, filename = argv
 
+# open the file (filename will be the name of the file)
 txt = open(filename)
 
+# print stuff
 print("Here's your file %r: " %filename)
 
+# print what's inside the file
 print(txt.read())
 
+# ask the user to type the filename again
 print("Type the filename again:")
 file_again = input("> ")
 
+# open the file and print what is inside
 txt_again = open(file_again)
 print(txt_again.read())
 
