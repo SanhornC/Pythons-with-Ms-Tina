2e74a80b7d0af39919edb43bdb1d8cb77b44485b
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-29 00:20:13 +0800

    exercise 12

--- ex12.py
+++ ex12.py
@@ -4,3 +4,5 @@ weight = input("How much do you weigh?")
 
 print("So, you're %r old, %r tall and %r heavy" %(age, height, weight))
 
+# drill 2
+# The pydoc module automatically generates documentation from Python modules. The documentation can be presented as pages of text on the console, served to a Web browser, or saved to HTML files.
