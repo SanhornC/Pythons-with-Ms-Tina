d91c18dd16c4356097a742d499d5adf723efdc95
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-28 22:27:31 +0800

    exercise 1

---
+++ ex1.py
@@ -0,0 +1,20 @@
+#  this character can allow programmer to write commmand without interfering running the code
+
+
+#print("sooo boring")
+#print("this is the first exercise")
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+
+
+
+# print another line: study drill 1
+
+print("I'am really smart")
+
