80cfbb30ca499d9cc630c144b96571f7674e5b96
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-29 00:18:14 +0800

    exercise 6

--- ex6.py
+++ ex6.py
@@ -1,18 +1,18 @@
 types_of_people = 10
-x = f"There are {types_of_people} types of people."
+x = f"There are {types_of_people} types of people." # string is put inside string
 # there are 10 types of people
 # f"" is similar to str.format
 binary = "binary"
 do_not = "don't"
-y = f"Those who know {binary} and those who {do_not}."
+y = f"Those who know {binary} and those who {do_not}." # string is put inside string
 
 # print x and y
 print(x)
 print(y)
 
 # print the following sentences (formated)
-print(f"I said: {x}")
-print(f"I also said: '{y}'")
+print(f"I said: {x}") # sting is put inside string
+print(f"I also said: '{y}'") # string is put inside string
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
