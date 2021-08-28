0b2914fb3a70afc37227d7623016c93a0ba1ed01
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-28 23:25:12 +0800

    exercise 6

--- ex6.py
+++ ex6.py
@@ -1,13 +1,16 @@
 types_of_people = 10
 x = f"There are {types_of_people} types of people."
-
+# there are 10 types of people
+# f"" is similar to str.format
 binary = "binary"
 do_not = "don't"
 y = f"Those who know {binary} and those who {do_not}."
 
+# print x and y
 print(x)
 print(y)
 
+# print the following sentences (formated)
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
