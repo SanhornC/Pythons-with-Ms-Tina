de76d92e2cc68e64d7204141514177c0819e7fda
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-08-28 22:38:39 +0800

    exercise 3

---
+++ ex3.py
@@ -0,0 +1,34 @@
+# print stuff
+print("I will now count my chickens:")
+
+# print hens and 30.0
+print("Hens", 25.0 + 30.0 / 6.0)
+
+# print roosters and 97.0
+print("Roosters", 100.0 - 25.0 * 3.0 % 4)
+
+# print the line
+print("Now I will count the eggs:")
+
+print(3 + 2.0 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# print true or false
+print(3 + 2 < 5 - 7.0)
+
+# print line and 5.0
+print("What is 3 + 2?", 3.0 + 2)
+
+# print line and -2.0
+print("What is 5 - 7?", 5.0 - 7)
+
+# print stuff
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# print out true or false 
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2)
+print("Is it less or equal?", 5 <= -2.0)
