135e9d51dc6d0c752838fb1f5075b48746eecf2c
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-09-01 14:04:31 +0800

    exercise 28

---
+++ ex28.py
@@ -0,0 +1,49 @@
+'''
+1. true
+2. false
+3. false
+4. true
+5. true
+6. true
+7. False
+8. True
+9. False
+10. False
+11. True
+12. False
+13. True
+14. False
+15. False
+16. False
+17. True
+18. True
+19. False
+20. false
+
+'''
+
+
+print(True and True)
+print(False and True) 
+print(1 == 1 and 2 == 1) # equal and equal
+print("test" == "test") # equal
+print(1 == 1 or 2 != 1) # equal or not equal
+print(True and 1 == 1) # equal
+print(False and 0 != 0) # not equal
+print(True or 1 == 1) # equal
+print("test" == "testing") # equal
+print(1 != 0 and 2 == 1) # not equal and equal
+print("test" != "testing") # not equal
+print("test" == 1) # equal
+print(not(True and False)) 
+print(not (1 == 1 and 0 != 1)) # equal and not equal
+print(not (10 == 1 or 1000 == 1000)) # equal or equal
+print(not (1 != 10 or 3 == 4)) # not equal or equal
+print(not ("testing" == "testing" and "Zed" == "Cool Guy")) # equal
+print(1 == 1 and not ("testing" == 1 or 1 == 0)) # equal
+print("chunky" == "bacon" and not (3 == 4 or 3 == 3)) # equal
+print(3 == 3 and not ("testing" == "testing" or "Python" == "Fun")) # equal
+
+print("---------------------------------------------------------------------")
+print("Study drill 3")
+print(5 <= 10)
