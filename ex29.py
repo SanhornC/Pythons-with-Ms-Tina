c880ea91492497d3d0b6f455ca96a44d21cfcdf5
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-09-01 14:38:05 +0800

    exercise 29

---
+++ ex29.py
@@ -0,0 +1,47 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+
+if people < dogs:
+    print("The world is doorled on!")
+
+
+if people > dogs:
+    print("The world is dry!")
+
+
+
+dogs+=5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs.")
+
+
+i = 99
+j = 199
+if i < j:
+    print("99 is smaller than 199")
+
+'''
+study drill 1: set conditions, if the conditions are met, then it will implement the followings
+study drill 2: or else it won't be able to run the code
+study drill 3: python will fail to determine what code to run
+study drill 5: if we change values in different variables, then the output will be changed
+'''
