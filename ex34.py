013f2d948e3422d3d3025f4d8eb07c4e2642a457
Sanhorn <53225164+SanhornC@users.noreply.github.com>
2021-09-03 09:56:25 +0800

    exercise 34

---
+++ ex34.py
@@ -0,0 +1,29 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+
+print(animals[1])
+
+print(animals[2])
+
+print(animals[0])
+
+print(animals[3])
+
+print(animals[4])
+
+print(animals[2])
+
+print(animals[5])
+
+print(animals[4])
+
+# study drill 3
+students = ["pig", "dog", "cat", "rat", "horse"]
+
+for i in range (0,5,2):
+    print(students[i] + " ", end = " ")
+
+'''
+Study drill 1: ordinal number counts from 1; cardinal number counts from 0
+Study drill 2: cuz there is no year 0, so 2010 will still be 2010
+
+'''
